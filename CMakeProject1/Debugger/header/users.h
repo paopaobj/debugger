a7a393ab7daa01d3eb7ff4e91feb1718e40766e5
Junhong Zhang <145085327+paopaobj@users.noreply.github.com>
2024-11-23 00:25:52 +0800

    Add files via upload

--- CMakeProject1/Debugger/header/users.h
+++ CMakeProject1/Debugger/header/users.h
@@ -15,20 +15,25 @@ public:
 		int line = debug.nowline[step];
 		return std::make_pair(vt, line);
 	}
-	void next() {
+	bool next() {
+		if (step == debug.nowline.size() - 1) return false;
 		step++;
+		return true;
 	}
-	void previous() {
+	bool previous() {
+		if (step == 0) return false;
 		step--;
+		return true;
 	}
-	void jump(int line) {
+	bool jump(int line) {
 		std::vector<int> lines = debug.nowline;
 		for (int i = 0; i < lines.size(); i++) {
 			if (lines[i] == line) {
 				step = i;
-				break;
+				return true;
 			}
 		}
+		return false;
 	}
 };
 
